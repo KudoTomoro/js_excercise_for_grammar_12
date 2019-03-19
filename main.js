4b2978be333ddca5b10ebb65979364ad90683374
KudoTomoro <tomoro_5656_onepiece@ezweb.ne.jp>
2019-03-19 17:50:37 +0800

    excercise

--- main.js
+++ main.js
@@ -14,7 +14,37 @@
  *         - username : 返信ツイートしたユーザー名(文字列。ユーザー名は自由につけて良い)
  *         - text : ツイート内容(文字列。ツイート内容は自由に作って良い。)
  */
+const tweets = [
+  {
+    username:'tomoro',
+    text:'aaaaaaaaaaaaaaaaa',
+    replies:[
+      {
+        username:'mercari',
+        text:'bbbbbbbbbbbbbbbbbbb'
+      },
+      {
+        username:'line',
+        text:'bbbbbbbbbbbbbbbbbbb'
+      },
+    ]
+  },
 
+  {
+    username:'kudo',
+    text:'cccccccccccccccccccc',
+    replies:[
+      {
+        username:'dmm',
+        text:'dddddddddddddddddddddd'
+      },
+      {
+        username:'gunocy',
+        text:'dddddddddddddddddddddd'
+      },
+    ]
+  },
+]
 
 /**
  * 課題2: 課題1で作ったtweetsの内容を全て表示する
@@ -24,3 +54,13 @@
  *     - replies
  *       - repliesの内容もforEachを使って全て表示する
  */
+
+tweets.forEach((tweet,index) => {
+  console.log(tweet.username);
+  console.log(tweet.text);
+  // console.log(tweet.replies);
+  tweet.replies.forEach((reply,index) => {
+    console.log(reply.username);
+    console.log(reply.text);
+  });
+});
